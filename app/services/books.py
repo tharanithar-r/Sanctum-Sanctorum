b9c3de7731533e83b6338630705bc4d611d577f1
Tharanithar R <tharanithar@Tharanithars-MacBook-Air.local>
2026-09-20 02:56:04 +0530

    Match search query against author as well as title

--- app/services/books.py
+++ app/services/books.py
@@ -2,7 +2,7 @@
 from typing import Optional
 
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import or_, select
 from sqlalchemy.orm import Session
 
 from app.models import Book
@@ -59,7 +59,12 @@ def list_books(
     """
     query = select(Book)
     if q:
-        query = query.where(Book.title.icontains(q, autoescape=True))
+        query = query.where(
+            or_(
+                Book.title.icontains(q, autoescape=True),
+                Book.author.icontains(q, autoescape=True),
+            )
+        )
     if restricted is not None:
         query = query.where(Book.restricted == restricted)
     # TODO: min_price / max_price filters
